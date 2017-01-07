6c5b00361cb68f103a5bc2016efc4d7b4570a975
mkarmowski <mateusz@karmowski.com>
2017-01-07 18:10:01 +0100

    basic templates

--- transactions/views.py
+++ transactions/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Transaction, Category
 
-def transaction_detail(request, id)
+
+def transaction_detail(request, id):
     transaction = get_object_or_404(Transaction, pk=id)
     return render(request, 'transaction/detail.html', {'transaction': transaction})
+
+
+def transaction_list(request):
+    transactions = Transaction.objects.all()
+    return render(request, 'transaction/list.html', {'transactions': transactions})

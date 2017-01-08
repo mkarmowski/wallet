61a45e70929b7d8c0bf919642ea189aebb9e3b6e
mkarmowski <mateusz@karmowski.com>
2017-01-08 12:52:25 +0100

    index

--- wallet/urls.py
+++ wallet/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^transactions/', include('transactions.urls')),
-    url(r'^', include('users.urls')),
     url(r'^users/', include('users.urls')),
-
+    url(r'^$', TemplateView.as_view(template_name='users/index.html')),
 ]

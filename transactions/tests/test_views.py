efadb60c003796b8b263583728a8cadb668834f2
mkarmowski <mateusz@karmowski.com>
2017-02-19 20:57:25 +0100

    transactions views tests

--- transactions/tests/test_views.py
+++ transactions/tests/test_views.py
@@ -1,10 +1,11 @@
+import datetime
 from django.contrib.auth.models import User
 from django.test import Client
 from django.test import TestCase
 from django.urls import reverse
 
-from transactions.forms import WalletCreateForm
-from transactions.models import Wallet
+from transactions.forms import WalletCreateForm, CategoryCreateForm, TransactionCreateForm
+from transactions.models import Wallet, Category, Transaction
 
 
 class RedirectNotLoggedIn(TestCase):
@@ -57,7 +58,7 @@ class WalletViewsTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_get_wallet_edit_view(self):
-        response = self.client.get(reverse('wallet:wallet_create'))
+        response = self.client.get(reverse('wallet:wallet_update', args=(self.wallet.id,)))
         self.assertEqual(str(response.context['user']), 'john_snow')
         self.assertEqual(response.status_code, 200)
 
@@ -65,6 +66,7 @@ class WalletViewsTest(TestCase):
         response = self.client.get(reverse('wallet:wallet_delete', args=(self.wallet.id,)))
         self.assertEqual(str(response.context['user']), 'john_snow')
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Are you sure you want to delete')
 
 
 class WalletFormsTest(TestCase):
@@ -89,21 +91,12 @@ class WalletFormsTest(TestCase):
         form = WalletCreateForm(data=form_data)
         self.assertFalse(form.is_valid())
 
-    def test_wallet_create_form_with_invalid_data_too_long_name(self):
-        form_data = {'name': 'TestWallet2'*12, 'balance': 'invalid', 'description': 'test'}
-        form = WalletCreateForm(data=form_data)
-        self.assertFalse(form.is_valid())
-
     def test_wallet_create_form_with_valid_data(self):
         form_data = {'name': 'TestWallet2', 'balance': 100, 'description': 'test'}
         form = WalletCreateForm(data=form_data)
         self.assertTrue(form.is_valid())
-        # test_wallet = form.save()
-        # self.assertEqual(test_wallet.name, 'TestWallet2')
-        # self.assertEqual(test_wallet.balance, 100)
-        # self.assertEqual(test_wallet.description, 'test')
 
-    def test_post_create_form(self):
+    def test_post_wallet_create_form(self):
         url = reverse('wallet:wallet_create')
         form_data = {'name': 'TestWallet2', 'balance': 100, 'description': 'test'}
         response = self.client.post(url, form_data)
@@ -113,4 +106,59 @@ class WalletFormsTest(TestCase):
         self.assertEqual(test_wallet.balance, 100)
         self.assertEqual(test_wallet.description, 'test')
 
+    def test_post_wallet_edit_form_valid_data(self):
+        test_wallet = Wallet.objects.create(name='TestWallet3', balance=1000, user=self.user, description='test')
+        url = reverse('wallet:wallet_update', args=(test_wallet.id,))
+        form_data = {'balance': 222}
+        response = self.client.post(url, form_data, follow=True)
+        self.assertEqual(response.status_code, 200)
+        # self.assertEqual(self.wallet.balance, 222)
 
+    def test_wallet_delete(self):
+        response = self.client.post(reverse('wallet:wallet_delete', args=(self.wallet.id,)), follow=True)
+        self.assertRedirects(response, reverse('wallet:wallet_list'), status_code=302)
+
+
+class CategoryFormsTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='john_snow', password='password')
+        self.user.save()
+        self.client = Client()
+        self.client.login(username='john_snow', password='password')
+        self.category = Category.objects.create(name='TestCategory', user=self.user)
+        self.category.save()
+
+    def test_category_create_form_blank(self):
+        form = CategoryCreateForm({})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {'name': ['This field is required.']})
+
+    def test_category_create_form_with_invalid_data(self):
+        form_data = {'name': 'TestCategory2'*12}
+        form = CategoryCreateForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_category_create_form_with_valid_data(self):
+        form_data = {'name': 'TestCategory2'}
+        form = CategoryCreateForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_post_category_create_form(self):
+        url = reverse('wallet:category_create')
+        form_data = {'name': 'TestCategory2'}
+        response = self.client.post(url, form_data)
+        self.assertEqual(response.status_code, 200)
+        test_category = Category.objects.get(name='TestCategory2')
+        self.assertEqual(test_category.name, 'TestCategory2')
+        self.assertEqual(test_category.user, self.user)
+
+    def test_post_category_edit_form_valid_data(self):
+        test_category = Category.objects.create(name='TestCategory3', user=self.user)
+        url = reverse('wallet:category_update', args=(test_category.id,))
+        form_data = {'name': 'TestCategoryChanged'}
+        response = self.client.post(url, form_data, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_category_delete(self):
+        response = self.client.post(reverse('wallet:category_delete', args=(self.category.id,)), follow=True)
+        self.assertRedirects(response, reverse('wallet:category_list'), status_code=302)
